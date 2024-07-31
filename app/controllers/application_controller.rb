7b93f62afed757365eb9d1d201a1bd6df5d29368
ayumukoda <168876414+ayumukoda@users.noreply.github.com>
2024-07-31 14:40:41 +0000

    名前をログイン、登録画面に追加、ログインアウト新規登録のリンク作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    #ログインやユーザ登録の前にconfigure_permitted_parameter（パラメーターを取得）が使われる
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+    
+    #devise_parameter_sanitizer.permitメソッドを使うことでユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可しています
+    def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
