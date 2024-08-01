bb7bdea9858607ccbf0df4a2ee75eb255f5d4793
ayumukoda <168876414+ayumukoda@users.noreply.github.com>
2024-08-01 07:12:05 +0000

    aboutページ作成、サインアウト後の遷移先、投稿画像用のテーブルを作成、モデルにimageを持たせた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
     #ログインやユーザ登録の前にconfigure_permitted_parameter（パラメーターを取得）が使われる
     before_action :configure_permitted_parameters, if: :devise_controller?
-
+    
+    def after_sign_in_path_for(resources)
+        about_path
+    end
+    
+    def after_sign_out_path_for(resources)
+        about_path
+    end
+    
     protected
     
     #devise_parameter_sanitizer.permitメソッドを使うことでユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可しています
     def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   #devise_for :usersはdevise を使用する際に URL として users を含むことを示している:
   root to: "homes#top"
-  #get 'homes/top'
+  #get 'homes/top'はデフォルトこれは消してroot toを使う
+  get 'homes/about' => 'homes#about', as: 'about'
+  #名前付きルートをaboutに設定
+  
 end
