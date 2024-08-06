14189f311057e6769343a0d2abeeab501a74d580
ayumukoda <168876414+ayumukoda@users.noreply.github.com>
2024-08-06 13:17:46 +0000

    投稿一覧を見れるように機能を追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,4 +3,13 @@ class PostImage < ApplicationRecord
     has_one_attached :image
     belongs_to :user
     #1:N　N側（ユーザー1人に対しポストは何投稿もできる
+    
+    #get_imageメゾットで画像を表示させる（設定なしならno_imageありならimageを持ってくる
+    def get_image
+        unless image.attached?
+            file_path = Rails.root.join('app/assets/images/no_image.jpg')
+            image.attach(io: File.open(file_path), filename: 'default-image.jpeg', content_type: 'image/jpeg')
+        end
+            image
+    end
 end
