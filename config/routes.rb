5af5ef589a89fc7a3de2069073e5fa5bd9faf76a
ayumukoda <168876414+ayumukoda@users.noreply.github.com>
2024-08-07 22:55:22 +0000

    userモデルでactivestorageを使えるように、userコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  #get 'post_images/new'
-  #get 'post_images/index'
-  #get 'post_images/show'
   devise_for :users
   #devise_for :usersはdevise を使用する際に URL として users を含むことを示している:
   root to: "homes#top"
   #get 'homes/top'はデフォルトこれは消してroot toを使う
   get 'homes/about' => 'homes#about', as: 'about'
   #名前付きルートをaboutに設定
+  resources :users, only: [:show, :edit]
+  #get 'users/show'
+  #get 'users/edit'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  #get 'post_images/new'
+  #get 'post_images/index'
+  #get 'post_images/show'
 end
